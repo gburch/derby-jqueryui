3ee301a5bace76d309eb4935c6345356636f58df
Greg Burch <greg@sliderocket.com>
2012-10-18 17:46:02 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var juiRoot = __dirname + '/node_modules/jquery-ui/',
+	config = {
+		ns: 'jui',
+		filename: __filename,
+		scripts: {
+			jquery: require('./jquery-1.8.2.js'),
+	  	ui: require('./ui')
+		}
+};
+
+module.exports = jui;
+jui.decorate = 'derby';
+
+function jui(derby, options) {
+	var outConfig = Object.create(config),
+		scripts, outScripts, i, len, script, styles, outStyles, style;
+
+	if (options && 'styles' in options) {
+		styles = options.styles;
+		if (typeof styles === 'string') styles = [styles];
+		if (Array.isArray(styles)) {
+			outStyles = [];
+			for (i = 0, len = styles.length; i < len; i++) {
+				outStyles.push(juiRoot + styles[i]);
+			}
+		}
+	} else {
+		outStyles = juiRoot + './themes/base/jquery.ui.all';
+	}
+
+
+	if (options && 'scripts' in options) {
+		scripts = options.scripts;
+		if (typeof scripts === 'string') scripts = [scripts];
+		if (Array.isArray(scripts)) {
+			outScripts = [];
+			for (i = 0, len = scripts.length; i < len; i++) {
+				outScripts.push(juiRoot + scripts[i]);
+			}
+		}
+	} else {
+		outScripts = [juiRoot + './ui', juiRoot + './jquery-1.8.2.js'];
+	}
+
+	outConfig.scripts = outScripts;
+	outConfig.styles = outStyles;
+	derby.createLibrary(outConfig, options);
+	return this;
+}
